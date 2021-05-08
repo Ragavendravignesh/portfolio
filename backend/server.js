748c294aec77eb2d062a97d135a8cfe17f446e72
Ragavendra S <ragavendrabest77@gmail.com>
2021-05-08 17:06:26 +0530

    Mongodb connection added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,20 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+
+dotenv.config()
+
+connectDB()
 
 const app = express()
 
-app.listen(5000, console.log('App is listening at port 5000'))
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(
+    `Server is running at ${process.env.NODE_ENV} mode at port ${process.env.PORT}`
+  )
+)
