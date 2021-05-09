fdeb4d3bd4a317c0b1875c14086fc116f7a2e3bd
Ragavendra S <ragavendrabest77@gmail.com>
2021-05-09 17:43:56 +0530

    Store file added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,7 +6,9 @@ const Header = () => {
   return (
     <Navbar variant='dark' bg='dark' expand='lg' collapseOnSelect>
       <Container>
-        <Navbar.Brand href='#home'>Portfolio</Navbar.Brand>
+        <LinkContainer to='/'>
+          <Navbar.Brand>Portfolio</Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
           <Nav className='ml-auto'>
