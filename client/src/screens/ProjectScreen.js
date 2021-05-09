e29019cf2e1ab0a1049b4630c11b3e46385108d0
Ragavendra S <ragavendrabest77@gmail.com>
2021-05-09 19:30:55 +0530

    Project screen added

--- client/src/screens/ProjectScreen.js
+++ client/src/screens/ProjectScreen.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import ProjectCard from '../components/ProjectCard'
+import { getProjects } from '../actions/projectActions'
+import { Row, Col } from 'react-bootstrap'
 
 const ProjectScreen = () => {
-    return (
-        <div>
-            This is project screen
-        </div>
-    )
+  const dispatch = useDispatch()
+
+  const projectDetails = useSelector((state) => state.projectDetails)
+  const { loading, projects, error } = projectDetails
+
+  useEffect(() => {
+    dispatch(getProjects())
+  }, [dispatch])
+
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>
+          {projects.map((project) => (
+            <Col key={project._id} sm={12} md={6} lg={4} xl={3}>
+              <ProjectCard project={project} />
+            </Col>
+          ))}
+        </Row>
+      )}
+    </>
+  )
 }
 
 export default ProjectScreen
