fd93bd9b944563ea5a175fc9a64bbd160c3c2171
Ragavendra S <ragavendrabest77@gmail.com>
2021-05-09 19:27:38 +0530

    projectcard added

--- client/src/components/ProjectCard.js
+++ client/src/components/ProjectCard.js
@@ -1,20 +1,28 @@
 import React from 'react'
-import { Card } from 'react-bootstrap'
+import { Card, Row, Col } from 'react-bootstrap'
 
 const ProjectCard = ({ project }) => {
   return (
-    <Card className='p-3 my-3 rounded'>
-      <Card.Img src={project.image} variant='top'></Card.Img>
+    <Card className='h-100 p-3 my-4 rounded'>
+        {console.log(project.image)}
+      <Card.Img src={`${project.image}`} variant='top'></Card.Img>
       <Card.Title>{project.name}</Card.Title>
-      <Card.Text>Destination: {project.description}</Card.Text>
-      <Card.Text>Tech Stack: {project.techStack}</Card.Text>
+      <Card.Text>
+        <strong>Description:</strong> {project.description}
+      </Card.Text>
+      <Card.Text>
+        <strong>Tech Stack:</strong> {project.techStack}
+      </Card.Text>
       <Row>
         <Col>
           <Card.Link href={`${project.githubLink}`}>
-            <i class='fab fa-lg fa-github'></i>
+            <i className='fab fa-lg fa-github'></i>
           </Card.Link>
+          {console.log(project.demoLink)}
           {project.demoLink && (
-            <Card.Link href={`${project.demoLink}`}>Demo</Card.Link>
+            <>
+              <Card.Link href={`${project.demoLink}`}>Demo</Card.Link>
+            </>
           )}
         </Col>
       </Row>
