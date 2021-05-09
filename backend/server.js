d8cfb0fe13779275ee41fa9fa111f5dd14a8458a
Ragavendra S <ragavendrabest77@gmail.com>
2021-05-10 00:53:03 +0530

    Ready for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import path from 'path'
 import morgan from 'morgan'
 
 import projectRoutes from './routes/projectRoutes.js'
@@ -24,6 +25,20 @@ if(process.env.NODE_ENV === 'development') {
 app.use('/api/projects', projectRoutes);
 app.use('/api/blogs', blogRoutes);
 app.use('/api/mail', emailRoutes);
+
+const __dirname = path.resolve()
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname,'/client/build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
+}
 
 app.use(notFound)
 app.use(errorHandler)
